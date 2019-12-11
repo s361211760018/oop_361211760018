f1c5a881e87000637ae8405d5ac6df73bea06329
jaruwan <zeejaruwan27@gmail.com>
2019-12-11 09:06:00 +0700

    Test

--- src/lab1/Hello.java
+++ src/lab1/Hello.java
@@ -3,7 +3,9 @@ package lab1;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Word,i am zee");
+        System.out.println("361211760018");
         System.out.println("MIT");
+        System.out.println("Faculty of Management Tectnology");
 
     } //main
 
