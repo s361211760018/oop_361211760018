b66e731a6b7548b462a086745666b0427e8928d5
jaruwan <zeejaruwan27@gmail.com>
2019-12-11 08:59:25 +0700

    Initial commit

---
+++ src/lab1/Hello.java
@@ -0,0 +1,11 @@
+package lab1;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Word,i am zee");
+        System.out.println("MIT");
+
+    } //main
+
+
+} //class
